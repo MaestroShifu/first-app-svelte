f8f63490312bbcb57937fa207926c2d3d1e685fa
MaestroShifu <sdanielsarsantos@gmail.com>
2021-11-25 22:33:11 -0500

    add datejs

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,20 @@
   import Form from './ui/Form.svelte';
   import List from './ui/List.svelte';
   import ListPhotos from './ui/ListPhotos.svelte';
+  import dayjs from 'dayjs';
+  import utc from 'dayjs/plugin/utc';
+
+  dayjs.extend(utc);
+
+  const dateNow = dayjs().format();
+  const dateNowUTC = dayjs().utc().format();
 </script>
 
 <main>
+  <span class="date-now">Fecha con dayjs: {dateNow}</span>
+  <br />
+  <span class="date-now-utc">Fecha con dayjs UTC 0: {dateNowUTC}</span>
+  <br />
   <Form />
   <br />
   <List />
@@ -13,4 +24,11 @@
 </main>
 
 <style>
+  .date-now {
+    color: blue;
+  }
+
+  .date-now-utc {
+    color: seagreen;
+  }
 </style>
